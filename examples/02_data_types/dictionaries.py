3c3ae426f3d9979d0a89f905faf5a495db4bb6ed
Dhruvi Raval <dhruviraval13110@gmail.com>
2026-07-23 13:06:48 +0530

    Add example of dictionary usage in Python

---
+++ examples/02_data_types/dictionaries.py
@@ -0,0 +1,17 @@
+"""
+Dictionaries in Python.
+"""
+
+student = {
+    "name": "Dhruvi",
+    "age": 20,
+    "city": "Ahmedabad"
+}
+
+print(student)
+
+print(student["name"])
+
+student["age"] = 21
+
+print(student)
