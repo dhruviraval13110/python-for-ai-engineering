9e4ddf8832729e69f2ec0587285c85c898e298c3
Dhruvi Raval <dhruviraval13110@gmail.com>
2026-07-23 12:37:24 +0530

    Add project constants to constants.py

---
+++ src/constants.py
@@ -0,0 +1,15 @@
+"""
+Project constants.
+"""
+
+APP_NAME = "Python for AI Engineering"
+
+VERSION = "0.1.0"
+
+AUTHOR = "Dhruvi Raval"
+
+DEFAULT_ENCODING = "utf-8"
+
+WELCOME_MESSAGE = (
+    "Welcome to Python for AI Engineering!"
+)
