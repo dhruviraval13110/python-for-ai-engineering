80fe672bbc04c5723fc0d673f5b7153afaadca25
Dhruvi Raval <dhruviraval13110@gmail.com>
2026-07-23 12:42:09 +0530

    Add tests for main application execution

---
+++ tests/test_main.py
@@ -0,0 +1,12 @@
+"""
+Tests for the main application.
+"""
+
+from src.main import main
+
+
+def test_main_runs_without_error():
+    """
+    Verify the application executes without raising exceptions.
+    """
+    main()
