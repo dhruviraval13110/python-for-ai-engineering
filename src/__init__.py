ecbee50fe95777ad86815fcd0815579fa5ad5db9
Dhruvi Raval <dhruviraval13110@gmail.com>
2026-07-23 12:36:51 +0530

    Initialize __init__.py with project metadata

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+"""
+Python for AI Engineering
+
+A collection of Python fundamentals and engineering best practices
+for Artificial Intelligence and Machine Learning.
+"""
+
+__version__ = "0.1.0"
+
+__author__ = "Dhruvi Raval"
+
+__license__ = "MIT"
