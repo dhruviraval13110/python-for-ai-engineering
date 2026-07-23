8d6ade0ff47fc9950528f76d5dfe4f33326bbed2
Dhruvi Raval <dhruviraval13110@gmail.com>
2026-07-23 13:07:58 +0530

    Create exercises.py

---
+++ examples/02_data_types/exercises.py
@@ -0,0 +1,9 @@
+"""
+Practice Exercises
+
+1. Create a list of your favorite books.
+2. Create a tuple containing latitude and longitude.
+3. Create a dictionary for your profile.
+4. Create a set of unique numbers.
+5. Print the type of every variable.
+"""
