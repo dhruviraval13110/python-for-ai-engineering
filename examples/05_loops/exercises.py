525124a1450191e1e83f73f844e44c571d5b5a59
Dhruvi Raval <dhruviraval13110@gmail.com>
2026-07-23 20:42:23 +0530

    Create exercises.py

---
+++ examples/05_loops/exercises.py
@@ -0,0 +1,23 @@
+"""
+Practice Questions
+"""
+
+# 1 Print numbers from 1 to 20
+
+# 2 Print even numbers
+
+# 3 Print odd numbers
+
+# 4 Find factorial
+
+# 5 Sum of first N numbers
+
+# 6 Reverse a number
+
+# 7 Check palindrome
+
+# 8 Multiplication table
+
+# 9 Star Pattern
+
+# 10 Prime Numbers
