895fa4850694401f1b9518310350b148aa7c0c24
Dhruvi Raval <dhruviraval13110@gmail.com>
2026-07-23 13:27:53 +0530

    Create for_loop.py

---
+++ examples/05_loops/for_loop.py
@@ -0,0 +1,13 @@
+"""
+Examples of for loops.
+"""
+
+fruits = ["Apple", "Banana", "Mango"]
+
+for fruit in fruits:
+    print(fruit)
+
+print("\nNumbers from 1 to 5:")
+
+for number in range(1, 6):
+    print(number)
