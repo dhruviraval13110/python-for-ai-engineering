38f0f9213d8ddbe8b9e4c0135fb8ae2676d5d5dc
Dhruvi Raval <dhruviraval13110@gmail.com>
2026-07-23 13:20:21 +0530

    Create conditional_expression.py

---
+++ examples/Conditions/conditional_expression.py
@@ -0,0 +1,15 @@
+"""
+Conditional (ternary) operator.
+"""
+
+age = 20
+
+status = "Adult" if age >= 18 else "Minor"
+
+print(status)
+
+number = 7
+
+result = "Even" if number % 2 == 0 else "Odd"
+
+print(result)
