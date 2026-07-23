4e8e80be45108ff1e59f2ad8309ef4cd2e02007a
Dhruvi Raval <dhruviraval13110@gmail.com>
2026-07-23 13:10:14 +0530

    Add arithmetic operators example in arithmetic.py

---
+++ examples/03_operators/arithmetic.py
@@ -0,0 +1,14 @@
+"""
+Arithmetic Operators
+"""
+
+a = 15
+b = 4
+
+print(f"Addition: {a + b}")
+print(f"Subtraction: {a - b}")
+print(f"Multiplication: {a * b}")
+print(f"Division: {a / b}")
+print(f"Floor Division: {a // b}")
+print(f"Modulus: {a % b}")
+print(f"Exponent: {a ** b}")
