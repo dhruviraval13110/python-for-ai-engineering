12aebff1b314a8b13cc74f7065e8f41ca1803976
Dhruvi Raval <dhruviraval13110@gmail.com>
2026-07-23 13:14:51 +0530

    Create exercises.py

---
+++ examples/03_operators/exercises.py
@@ -0,0 +1,9 @@
+"""
+Practice Exercises
+
+1. Calculate the area of a rectangle.
+2. Swap two variables without using a third variable.
+3. Check if a number is even or odd.
+4. Compare two user-entered numbers.
+5. Demonstrate every logical operator.
+"""
